c9def7437772045e5254270ac38a24d1e03484c4
juanchodavid15112004-netizen <juanchodavid15112004@gmail.com>
2026-08-05 23:12:52 -0400

    Delete _gen.js

--- _gen.js
+++
@@ -1,9 +0,0 @@
-﻿const fs = require('fs');
-const html = fs.readFileSync('C:/Users/juanc/Downloads/YOQUESE/index.html', 'utf8');
-const parts = [];
-let i = html.indexOf('Esta oferta termina en:');
-if (i >= 0) parts.push('COUNTDOWN:\n' + html.slice(i, i + 1800));
-i = html.indexOf('bg-white/10 backdrop-blur-sm rounded-2xl p-6 inline-block');
-if (i >= 0) parts.push('COUNTDOWN2:\n' + html.slice(i, i + 1800));
-parts.push('TAIL:\n' + html.slice(-800));
-fs.writeFileSync('C:/Users/juanc/Downloads/YOQUESE/snippets.txt', parts.join('\n'), 'utf8');
